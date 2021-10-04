2da420b2b68ada7fb95c74545e8dfd85f8405d72
mukitcoder <mukitbd71@gmail.com>
2021-10-04 14:39:20 +0600

    add fontawesome

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,87 @@
-import React from 'react';
-import './Footer.css';
+import React from "react";
+import "./Footer.css";
+import footerLogo from "../../images/footer.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faTwitter,
+  faYoutube,
+} from "@fortawesome/free-brands-svg-icons";
+
+const insta = <FontAwesomeIcon icon={faInstagram} />;
+const fb = <FontAwesomeIcon icon={faFacebook} />;
+const tweet = <FontAwesomeIcon icon={faTwitter} />;
+const yt = <FontAwesomeIcon icon={faYoutube} />;
 
 const Footer = () => {
-    return (
-        <div>
-            <h2>Footer</h2>
+  return (
+    <div className="footer-section text-white text-center">
+      <div className="row">
+        <div className="col-md-3">
+          <div className="p-4">
+            <img className="img-fluid w-75 my-3" src={footerLogo} alt="" />
+            <p>
+              If you don’t set goals, how can you know what you want to achieve
+              and measure whether you have achieved it?
+            </p>
+          </div>
+          <div className="ms-3">
+            <a className="fs-2 me-4" href="#">
+              {fb}
+            </a>
+            <a className="fs-2 me-4" href="#">
+              {insta}
+            </a>
+            <a className="fs-2 me-4" href="#">
+              {tweet}
+            </a>
+            <a className="fs-2 me-4" href="#">
+              {yt}
+            </a>
+          </div>
         </div>
-    );
+        <div className="col-md-3 mt-5">
+          <h5>Quick Links</h5>
+          <hr />
+          <ul className="footer-list text-center pe-4">
+            <li>About Us</li>
+            <li className="py-3">Teacher</li>
+            <li className="">Pricing</li>
+            <li className="py-3">Courses</li>
+            <li className="">Contact</li>
+          </ul>
+        </div>
+        <div className="col-md-3 mt-5">
+          <h5>Useful Links</h5>
+          <hr />
+          <ul className="footer-list text-center pe-4">
+            <li>Privacy policy</li>
+            <li className="py-3">Terms and Conditions</li>
+            <li className="">Disclaimer</li>
+            <li className="py-3">Support</li>
+            <li className="">FQA</li>
+          </ul>
+        </div>
+        <div className="col-md-3 mt-5">
+          <h5 className="mb-3">Stay Connected</h5>
+          <hr />
+          <div class="input-group mb-3 mt-5 w-75 mx-auto">
+            <input
+              type="text"
+              class="form-control"
+              placeholder="Your Email"
+              aria-label="Recipient's username"
+              aria-describedby="basic-addon2"
+            />
+            <span class="btn btn-outline button-style" id="basic-addon2">
+              Send
+            </span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,13 @@
 import React, { useEffect, useState } from "react";
 import Banner from "../Banner/Banner";
 import "./Home.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlayCircle } from "@fortawesome/free-regular-svg-icons";
+import { faStar, faUsers } from "@fortawesome/free-solid-svg-icons";
+
+const star = <FontAwesomeIcon icon={faStar} />;
+const lessonIcon = <FontAwesomeIcon icon={faPlayCircle} />;
+const studentIcon = <FontAwesomeIcon icon={faUsers} />;
 
 const Home = () => {
   const [courses, setCourses] = useState([]);
@@ -20,21 +27,38 @@ const Home = () => {
       </div>
       <div className="row row-cols-1 row-cols-md-4 g-4 px-4">
         {courses.map((course) => (
-          <div key={course.id} className="col">
+          <div key={course.id} className="col shadow">
             <div className="card">
               <img src={course.img} className="card-img-top w-100 mx-auto" alt="..." />
               <div className="card-body">
                 <h5 className="card-title text-secondary fw-bolder"><abbr className="text-primary fw-bold">${course.price}</abbr>/Life Time</h5>
-                <h5 className="card-title">
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
-                </h5>
+                <h4 className="card-title fw-bold">
+                {course.title}
+                </h4>
                 <p className="card-text">
-                  This is a longer card with supporting text below as a natural
-                  lead-in to additional content. This content is a little bit
-                  longer.
+                  {course.description}
                 </p>
+                <div className="d-flex justify-content-between">
+                    <div>
+                       {lessonIcon} {course.lessons} Lessons
+                    </div>
+                    <div>
+                      {studentIcon} {course.students} Students
+                    </div>
+                </div>
+                <div className="d-flex justify-content-between align-items-center mt-3">
+                    <div className="d-flex justify-content-center"> 
+                       <p className="me-2">{course.rating}.0 </p>
+                       <small className="text-warning">{star}</small>
+                       <small className="text-warning">{star}</small>
+                       <small className="text-warning">{star}</small>
+                       <small className="text-warning">{star}</small>
+                       <small className="text-warning">{star}</small>
+                    </div>
+                    <div>
+                        <button className="btn btn-outline button-style">Enroll Now</button>
+                    </div>
+                </div>
               </div>
             </div>
           </div>
