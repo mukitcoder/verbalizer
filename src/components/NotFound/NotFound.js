fdc683ef495f196c21ec8ae58b423cad492d42d1
mukitcoder <mukitbd71@gmail.com>
2021-10-04 17:47:46 +0600

    create notfound page

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,12 +1,20 @@
-import React from 'react';
-import './NotFound.css';
+import React from "react";
+import { Link } from "react-router-dom";
+import "./NotFound.css";
 
 const NotFound = () => {
-    return (
-        <div>
-          <h3>404 not found</h3>  
-        </div>
-    );
+  return (
+    <div className="not-found">
+      <div className="text-dark text-center">
+        <h1 className="py-4 bg-warning fw-bold">
+          Sorry, the page not found !!!
+        </h1>
+      </div>
+      <div className="d-flex align-items-end justify-content-center h-75">
+      <Link to="/home"> <button className="btn btn-warning btn-lg not-found-btn fw-bolder">  Go Back </button></Link>
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
